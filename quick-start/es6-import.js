c1ebafdc7fe2690997e70f3d386ffa3a203c4ea2
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2020-02-17 15:50:24 -0600

    Create es6-import.js

---
+++ quick-start/es6-import.js
@@ -0,0 +1,7 @@
+// Import the Geocoder class from the module
+import { Geocoder } from '@tamu-gisc/common/utils/geometry/geoprocessing';
+
+const geocoder = new Geocoder({
+  ...params go here
+});
+
