850873f1f2417fb39e9ae7b987c22b74c3e9d67b
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2020-02-18 00:38:17 -0600

    Update es6-import.js

--- quick-start/es6-import.js
+++ quick-start/es6-import.js
@@ -2,6 +2,36 @@
 import { Geocoder } from '@tamu-gisc/common/utils/geometry/geoprocessing';
 
 const geocoder = new Geocoder({
-  ...params go here
+  apiKey: 'YOUR_API_KEY_HERE',
+  version: '4.01',
+  streetAddress: '9355 Burton Way',
+  city: 'Beverly Hills',
+  state: 'ca',
+  zip: 99210
 });
 
+// Observable-based
+geocoder.execute()
+.subscribe((result) => {
+  // Receive successful result here
+}, (err) => {
+  // Handle XHR and non-success API error codes here
+});
+
+// Promise-based
+geocoder.execute(true)
+.then((result) => {
+  // Receive successful result here
+})
+.catch((err) => {
+  // Handle XHR and non-success API error codes here
+});
+
+// Callback
+geocoder.execute(function(err, result){
+  if(err){
+    // Handle XHR and non-success API error codes here
+  }
+  
+  // Receive successful result here
+});
