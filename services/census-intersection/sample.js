101a7e609e1b0f4bf766e735454971cfb60afdfa
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2022-08-02 04:29:14 -0500

    Added census intersection sample

---
+++ services/census-intersection/sample.js
@@ -0,0 +1,13 @@
+// Using our first-party JavaScript client library
+
+const intersection = new CensusIntersection({
+  apiKey: 'demo',
+  lat: 34.0726207996348,
+  lon: -118.397965182076,
+  s: 'CA',
+  censusYear: '2010',
+  version: '4.10'
+});
+
+
+// Click on the green arrow to execute the census intersection 
