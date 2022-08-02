bafa2390f679371df6b5ff5032444876cb632950
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2022-08-02 04:27:38 -0500

    Added reverse geocode sample

---
+++ services/reverse-geocoding/sample.js
@@ -0,0 +1,9 @@
+// Using our first-party JavaScript client library
+
+const geocode = new ReverseGeocoder({
+  apiKey: 'demo',
+  lat: 30.610487,
+  lon: -96.327766
+});
+
+// Click on the green arrow to execute the reverse geocode
