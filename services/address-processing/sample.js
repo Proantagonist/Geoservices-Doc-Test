bd84d56c2fcbe2a54aeaf505f541edd137c32165
Edgar Hernandez <Proantagonist@users.noreply.github.com>
2022-08-02 04:30:31 -0500

    Updated address processing sample spacing

--- services/address-processing/sample.js
+++ services/address-processing/sample.js
@@ -1,9 +1,9 @@
 // Using our first-party JavaScript client library
 
 const address = new AddressParser({
-      apiKey: 'demo',
-      version: '4.10',
-      nonParsedStreetAddress: '9355 Burton Way'
+  apiKey: 'demo',
+  version: '4.10',
+  nonParsedStreetAddress: '9355 Burton Way'
 });
 
 // Click on the green arrow to execute the geocode
